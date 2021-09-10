ed0e62dc08c75eb8e38ebf87fe7d029ef18b4663
Menna Mohamed <79084467+menna-anwer@users.noreply.github.com>
2021-09-10 10:39:54 +0200

    Update app.js

--- Landing Page/app.js
+++ Landing Page/app.js
@@ -1,13 +1,7 @@
 const navMenu = document.querySelector('#navbar__list');
 const navSections = document.querySelectorAll('section');
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
 
-// build the nav
 function buildNav() {
     const fragment = document.createDocumentFragment();
 
@@ -19,7 +13,7 @@ function buildNav() {
 
         // scroll to anchor ID using scroll to event
         aTag.addEventListener("click", () => {
-            navSection.scrollIntoView({behavior: "smooth"});
+            navSection.scrollIntoView({behavior: "auto"});
             });
         liTag.appendChild(aTag);
         fragment.appendChild(liTag);
