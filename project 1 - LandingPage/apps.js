b8251d10ab7ecbe9de525c77aab656728d01b544
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2023-01-25 11:22:08 +0200

    Add files via upload

---
+++ project 1 - LandingPage/apps.js
@@ -0,0 +1,26 @@
+const navMenu = document.querySelector('#navbar__list');
+const navSections = document.querySelectorAll('section');
+
+
+function buildNav() {
+    const fragment = document.createDocumentFragment();
+
+    navSections.forEach((navSection) => {
+        const liTag = document.createElement('li');
+        const aTag = document.createElement('a');
+        aTag.innerText = navSection.getAttribute('data-nav');
+        aTag.setAttribute('class', 'menu__link');
+
+        // scroll to anchor ID using scroll to event
+        aTag.addEventListener("click", () => {
+            navSection.scrollIntoView({behavior: "smooth"});
+            });
+        liTag.appendChild(aTag);
+        fragment.appendChild(liTag);
+    });
+    navMenu.appendChild(fragment);
+};
+
+
+// Build navigation menu
+buildNav();
